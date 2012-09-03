eb2f9b25710beb043184c6a4bbafe71496340ab6
Scott Epple <smepple@gmail.com>
2012-09-03 11:54:56 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Survivor::Application.config.secret_token = '74ce311c675d6203217e7c0ffc2573bebd56ba930741d5a8bd4b06ebcd9dfc9e01296c3dd4b571922b360081fcd7c65f31a723fe20c9e91e36e7bebe00d39339'
